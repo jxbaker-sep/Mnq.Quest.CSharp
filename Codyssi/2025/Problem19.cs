673833c88eddbd356abd5fad29ebbd42fa6c37e8
Jake Baker <SEP.Baker@fronius.com>
2025-08-15 10:09:23 -0400

    Codyssi.19.2-3

--- Codyssi/2025/Problem19.cs
+++ Codyssi/2025/Problem19.cs
@@ -32,25 +32,80 @@ public class Problem19
     (layerSums.Max() * layerSums.Count).Should().Be(expected);
   }
 
-  class Tree(Artifact Node, Tree? Left = null, Tree? Right = null)
+  [Theory]
+  [InlineData("Problem19.Sample.txt", "ozNxANO-pYNonIG-MUantNm-lOSlxki-SDJtdpa-JSXfNAJ")]
+  [InlineData("Problem19.txt", "hpqtovk-dRluceF-tsKfPZy-SfPcQfV-nOmLFZg-psWTuPA-gCppIei-bRfwRKE-JnsgGBC-BaSObRq-GhTUiRr-dWmHaDK-XcAWrhv-CqrQVvm")]
+  public void Part2(string inputFile, string expected)
+  {
+    var items = GetInput(inputFile)[..^2];
+
+    var root = new Tree(items[0]);
+    foreach(var item in items[1..]) root.Add(item);
+    var it = root.Add(new Artifact("missing", 500000));
+    (Parents(it) as IEnumerable<string>).Reverse().Join("-").Should().Be(expected);
+  }
+
+  List<string> Parents(Tree node)
+  {
+    Tree? it = node;
+    List<string> path = [];
+    it = it.Parent;
+    while (it != null)
+    {
+      path.Add(it.Code);
+      it = it.Parent;
+    }
+    return path;
+  }
+
+  Tree Find(Tree root, long id)
+  {
+    if (id == root.Id) return root;
+    else if (id < root.Id) return Find(root.Left ?? throw new ApplicationException(), id);
+    else return Find(root.Right ?? throw new ApplicationException(), id);
+  }
+
+  [Theory]
+  [InlineData("Problem19.Sample.txt", "pYNonIG")]
+  [InlineData("Problem19.txt", "YdnzjKz")] // JCSIQDT incorrect
+  public void Part3(string inputFile, string expected)
+  {
+    var items = GetInput(inputFile)[..^2];
+    var checks = GetInput(inputFile)[^2..];
+
+    var root = new Tree(items[0]);
+    foreach(var item in items[1..]) root.Add(item);
+    var p1 = Parents(Find(root, checks[0].Id)).ToHashSet();
+    var p2 = Parents(Find(root, checks[1].Id));
+    p2.First(it => p1.Contains(it)).Should().Be(expected);
+  }
+
+  class Tree(Artifact Node, Tree? Parent = null)
   {
     private Artifact Node { get; } = Node;
+    public Tree? Parent { get; } = Parent;
     public long Id { get; } = Node.Id;
     public string Code { get; } = Node.Code;
-    public Tree? Left { get; private set; } = Left;
-    public Tree? Right { get; private set; } = Right;
+    public Tree? Left { get; private set; } = null;
+    public Tree? Right { get; private set; } = null;
 
-    public void Add(Artifact item)
+    public Tree Add(Artifact item)
     {
       var hand = item.Id < Node.Id;
       if (item.Id < Node.Id)
       {
-        if (Left is {} l) l.Add(item);
-        else Left = new(item);
+        if (Left is {} l) return l.Add(item);
+        else {
+          Left = new(item, this);
+          return Left;
+        }
       }
       else {
-        if (Right is {} r) r.Add(item);
-        else Right = new(item);
+        if (Right is {} r) return r.Add(item);
+        else {
+          Right = new(item, this);
+          return Right;
+        }
       }
     }
   }
