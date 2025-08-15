16a675be9fd9ee8aac70b179dd5acf46a8493372
Jake Baker <SEP.Baker@fronius.com>
2025-08-15 12:42:28 -0400

    Codyssi.20.1

--- Codyssi/2025/Problem20.cs
+++ Codyssi/2025/Problem20.cs
@@ -2,45 +2,72 @@ using P = Parser.ParserBuiltins;
 using Parser;
 using Mng.Quest.CSharp.Utils;
 using Utils;
+using FluentAssertions;
 
 namespace Mnq.Quest.CSharp.Codyssi;
 
 public class Problem20
 {
   [Theory]
-  [InlineData("Problem20.Sample.txt", 12645822)]
-  // [InlineData("Problem20.txt", 1148860702)]
-  public void Part1(string inputFile, long expected)
+  [InlineData("Problem20.Sample.1.txt", 3, 201474)]
+  [InlineData("Problem20.Sample.2.txt", 80, 6902016000)]
+  [InlineData("Problem20.txt", 80, 244785900620800)]
+  public void Part1(string inputFile, int size, long expected)
   {
-    var items = GetInput(inputFile);
+    var world = GetInput(inputFile);
 
-    var instruction = new Instruction("FACE", 0, 1);
-    var cube = new CubeOfGrids(3);
-    Apply(cube, new Instruction("FACE", 0, 1));
-    cube.RotateLeft();
-    Apply(cube, new Instruction("FACE", 0, 2));
-    cube.Print();
+    var cube = new CubeOfGrids(size);
+    foreach(var (i, t) in world.Instructions.Zip(world.Twists))
+    {
+      Apply(cube, i);
+      // Console.WriteLine($"{cube.Facing} = {cube.Absorption(cube.Facing)}");
+      if (t == 'L') cube.RotateLeft();
+      else if (t == 'U') cube.RotateUp();
+      else if (t == 'R') cube.RotateRight();
+      else if (t == 'D') cube.RotateDown();
+      else throw new ApplicationException();
+    }
+    Apply(cube, world.Instructions[^1]);
+    // Console.WriteLine($"{cube.Facing} = {cube.Absorption(cube.Facing)}");
+
+    new[] { 1, -1, 2, -2, 3, -3 }
+      .Select(it => cube.Absorption(new Point3(0, it, 0)))
+      .OrderByDescending(it => it)
+      .Take(2)
+      .Product()
+      .Should().Be(expected);
+
+    // cube.Print();
   }
 
-  public static void Apply(CubeOfGrids cube, Instruction i)
+  private static void Apply(CubeOfGrids cube, Instruction i)
   {
-    if (i.Selector == "FACE")
-    {
-      var face = cube.Current;
-      for (var x = 0; x < cube.Size; x++)
-      {
-        for (var y = 0; y < cube.Size; y++)
-        {
-          face[y][x] = (y == x) ? (y * 3 + x) : i.Value;
+    var startRow = 0;
+    var startCol = 0;
+    var stopRow = cube.Size;
+    var stopCol = cube.Size;
+    var face = cube.Current;
+
+    if (i.Selector == "ROW") {
+      startRow = i.Index - 1;
+      stopRow = i.Index;
+    } else if (i.Selector == "COL") {
+      startCol = i.Index - 1;
+      stopCol = i.Index;
     }
+    
+    for (var x = startCol; x < stopCol; x++)
+    {
+      for (var y = startRow; y < stopRow; y++)
+      {
+        face[y][x] += i.Value;
       }
     }
-    else throw new NotImplementedException();
   }
 
   public class CubeOfGrids
   {
-    private Point3 Facing = new(1,2,3);
+    public Point3 Facing { get; private set; } = new(1,2,3);
     private readonly Dictionary<long, List<List<long>>> Faces = [];
     public int Size { get; }
 
@@ -73,7 +100,7 @@ public class Problem20
 
     public void RotateDown()
     {
-      Facing = Facing.RotateUp();
+      Facing = Facing.RotateDown();
       Roll(Facing.RotateRight(), Facing.RotateLeft());
     }
 
@@ -93,18 +120,23 @@ public class Problem20
     public void Print()
     {
       Console.WriteLine($"Facing = {Facing}");
-      Console.WriteLine($"Up:");
+      Console.WriteLine($"Up: {Absorption(Facing)}");
       PrintGrid(Facing);
-      Console.WriteLine($"Down:");
+      Console.WriteLine($"Down: {Absorption(Facing.RotateDown().RotateDown())}");
       PrintGrid(Facing.RotateDown().RotateDown());
-      Console.WriteLine($"Left:");
+      Console.WriteLine($"Left: {Absorption(Facing.RotateRight())}");
       PrintGrid(Facing.RotateRight());
-      Console.WriteLine($"Right:");
+      Console.WriteLine($"Right: {Absorption(Facing.RotateLeft())}");
       PrintGrid(Facing.RotateLeft());
-      Console.WriteLine($"Front:");
+      Console.WriteLine($"Front: {Absorption(Facing.RotateDown())}");
       PrintGrid(Facing.RotateDown());
-      Console.WriteLine($"Back:");
+      Console.WriteLine($"Back: {Absorption(Facing.RotateUp())}");
       PrintGrid(Facing.RotateUp());
+    }
+
+    public long Absorption(Point3 facing)
+    {
+      return Faces[facing.Y].Sum(it => it.Sum());
     }
 
     public void PrintGrid(Point3 facing)
@@ -122,6 +154,6 @@ public class Problem20
     var pps = CodyssiLoader.ReadAllText(inputFile).Paragraphs();
     var parser = P.Format("{} {} - VALUE {}", P.Word, P.Int.Optional(), P.Long)
       .Select(it => new Instruction(it.First, it.Second.Count > 0 ? it.Second[0] : 0, it.Third));
-    return new(parser.ParseMany(pps[0]), [.. pps[0][0]]);
+    return new(parser.ParseMany(pps[0]), [.. pps[1][0]]);
   }
 }
