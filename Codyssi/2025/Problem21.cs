836f4dc4c3c58285a934d7c21fd72bf1d9b5225f
Jake Baker <SEP.Baker@fronius.com>
2025-08-17 13:42:34 -0400

    Codyssi.21.3.wip

--- Codyssi/2025/Problem21.cs
+++ Codyssi/2025/Problem21.cs
@@ -51,7 +51,21 @@ public class Problem21
 
   [Theory]
   [InlineData("Problem21.Sample.1.txt", "1", "S1_0-S1_1-S1_2-S1_3-S1_4-S1_5-S1_6")]
+  [InlineData("Problem21.Sample.1.txt", "2", "S1_0-S1_1-S1_2-S1_3-S1_6")]
+  [InlineData("Problem21.Sample.1.txt", "3", "S1_0-S1_1-S1_2-S1_5-S1_6")]
+  [InlineData("Problem21.Sample.1.txt", "4", "S1_0-S1_1-S1_2-S2_2-S1_4-S1_5-S1_6")]
+  [InlineData("Problem21.Sample.1.txt", "5", "S1_0-S1_1-S1_2-S2_2-S2_3-S1_3-S1_4-S1_5-S1_6")]
+  [InlineData("Problem21.Sample.1.txt", "6", "S1_0-S1_1-S1_2-S2_2-S2_3-S1_3-S1_6")]
+  [InlineData("Problem21.Sample.1.txt", "7", "S1_0-S1_1-S1_2-S2_2-S2_3-S1_5-S1_6")]
+  [InlineData("Problem21.Sample.1.txt", "8", "S1_0-S1_1-S1_4-S1_5-S1_6")]
+  [InlineData("Problem21.Sample.1.txt", "9", "S1_0-S1_1-S2_3-S1_3-S1_4-S1_5-S1_6")]
   [InlineData("Problem21.Sample.1.txt", "10", "S1_0-S1_1-S2_3-S1_3-S1_6")]
+  [InlineData("Problem21.Sample.1.txt", "11", "S1_0-S1_1-S2_3-S1_5-S1_6")]
+  [InlineData("Problem21.Sample.1.txt", "12", "S1_0-S1_3-S1_4-S1_5-S1_6")]
+  [InlineData("Problem21.Sample.1.txt", "13", "S1_0-S1_3-S1_6")]
+  [InlineData("Problem21.Sample.1.txt", "14", "S1_0-S2_2-S1_4-S1_5-S1_6")]
+  [InlineData("Problem21.Sample.1.txt", "15", "S1_0-S2_2-S2_3-S1_3-S1_4-S1_5-S1_6")]
+  [InlineData("Problem21.Sample.1.txt", "16", "S1_0-S2_2-S2_3-S1_3-S1_6")]
   [InlineData("Problem21.Sample.1.txt", "17", "S1_0-S2_2-S2_3-S1_5-S1_6")]
   [InlineData("Problem21.Sample.1.txt", "100000000000000000000000000000", "S1_0-S2_2-S2_3-S1_5-S1_6")]
   [InlineData("Problem21.Sample.2.txt", "39", "S1_0-S1_1-S1_2-S2_3-S3_4-S3_5-S1_6")]
@@ -60,6 +74,7 @@ public class Problem21
   [InlineData("Problem21.Sample.3.txt", "100000000000000000000000000000", "S1_0-S1_6-S2_11-S2_17-S2_23-S2_29-S9_34-S9_37-S5_42-S5_48-S5_54-S5_60-S5_66-S5_72-S5_73-S5_74-S1_79-S3_84-S8_88-S8_89-S8_90-S3_90-S3_91-S1_96-S1_99")]
   [InlineData("Problem21.txt", "100000000000000000000000000000", "")]
   // Incorrect:
+  // f2-aa-3e-d4-29-96-94-5a-53-72-91-73-63-ec-7a-66
   // S1_0-S1_1-S1_2-S1_3-S1_4-S1_5-S1_6-S1_7-S1_8-S1_9-S1_10-S1_11-S1_12-S55_13-S55_15-S55_16-S55_17-S55_18-S1_19-S5_25-S1_31-S1_32-S1_33-S1_35-S10_37-S32_44-S32_47-S22_48-S22_51-S14_53-S14_54-S14_55-S14_56-S14_57-S2_57-S2_58-S2_60-S2_61-S2_62-S2_63-S6_64-S6_66-S6_68-S19_69-S19_70-S19_71-S19_72-S19_73-S19_74-S19_75-S19_77-S19_79-S43_80-S43_81-S43_82-S78_83-S78_84-S78_85-S147_85-S147_87-S147_88-S6_88-S6_91-S4_91-S4_92-S4_93-S4_94-S1_96-S1_97
   public void Part3(string inputFile, string goal_rank_exp, string expected)
   {
@@ -67,7 +82,7 @@ public class Problem21
     GetInput(inputFile);
     ComputeNumberOfPaths(Staircases[0], 0);
     var it = FindRank(Staircases[0], 0, 0, goal_rank);
-    it.Should().Be(expected);
+    it.Should().Be(expected, $"{CalculateMD5(it)}");
   }
 
   string FindRank(Staircase current, long step, BigInteger startingRank, BigInteger goalRank)
@@ -82,7 +97,7 @@ public class Problem21
     }
     var children = hashed_children.OrderBy(it => it.nextStaircase.Name).ThenBy(it => it.nextStep).ToList();
     string needle = "";
-    foreach(var (nextStaircase, nextStep) in children)
+    foreach (var (nextStaircase, nextStep) in children)
     {
       var np = ComputeNumberOfPaths(nextStaircase, nextStep);
       if (startingRank + np < goalRank)
@@ -101,7 +116,7 @@ public class Problem21
       }
       var (nextStaircase, nextStep) = children[^1];
       var np = ComputeNumberOfPaths(nextStaircase, nextStep);
-      needle = FindRank(nextStaircase, nextStep, startingRank - np, goalRank);
+      needle = FindRank(nextStaircase, nextStep, startingRank, goalRank);
     }
     return $"{current.Name}_{step}-{needle}";
   }
@@ -180,5 +195,14 @@ public class Problem21
     Staircases = parser.ParseMany(pps[0]);
     StaircaseMap = Staircases.ToDictionary(it => it.Name, it => it);
     Moves = P.Format("Possible Moves : {}", P.Long.Star(",")).Parse(pps[1][0]);
+  }
+
+  static string CalculateMD5(string data)
+  {
+    using (var md5 = MD5.Create())
+    {
+        var hash = md5.ComputeHash(data.Select(c => (byte)c).ToArray());
+        return BitConverter.ToString(hash).ToLowerInvariant();
+    }
   }
 }
