4e3765310194cd2f408ef6e497b72efcc523619b
Jake Baker <SEP.Baker@fronius.com>
2025-08-17 12:32:04 -0400

    Codyssi.21.2.wip

--- Codyssi/2025/Problem21.cs
+++ Codyssi/2025/Problem21.cs
@@ -5,6 +5,7 @@ using Utils;
 using Microsoft.Z3;
 using Mng.Quest.CSharp.Utils;
 using System.Numerics;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 
 namespace Mnq.Quest.CSharp.Codyssi;
 
@@ -21,49 +22,59 @@ public class Problem21
     GetInput(inputFile);
     Staircases = [Staircases[0]];
     ComputeNumberOfPaths(Staircases[0], 0)
-      .Should().Be(expected);
+    .Should().Be(expected);
+
+    // foreach (var path in Paths.Values.SelectMany(it => it).OrderBy(it => it)) Console.WriteLine(path);
+
   }
 
   private IReadOnlyList<long> Moves = [];
   private IReadOnlyList<Staircase> Staircases = [];
   private IReadOnlyDictionary<string, Staircase> StaircaseMap = new Dictionary<string, Staircase>();
 
-  // [Theory]
-  // [InlineData("Problem21.Sample.1.txt", "17")]
-  // [InlineData("Problem21.Sample.2.txt", "13")]
-  // [InlineData("Problem21.Sample.3.txt", "231843173048269749794")]
-  // [InlineData("Problem21.txt", "90448546029334024540396217")]
-  // public void Part2(string inputFile, string rep_expected)
-  // {
-  // BigInteger expected = BigInteger.Parse(rep_expected);
-  // var items = GetInput(inputFile);
-  // var sc = items.Staircases[0];
-  // ComputeNumberOfPaths(sc.Name, items.Staircases, items.Moves)
-  //   .Should().Be(expected);
-  // }
+  [Theory]
+  [InlineData("Problem21.Sample.1.txt", "17")]
+  // [InlineData("Problem21.Sample.2.txt", "102")]
+  // [InlineData("Problem21.Sample.3.txt", "113524314072255566781694")]
+  // [InlineData("Problem21.txt", "0")]
+  public void Part2(string inputFile, string rep_expected)
+  {
+    BigInteger expected = BigInteger.Parse(rep_expected);
+    GetInput(inputFile);
+    var result = ComputeNumberOfPaths(Staircases[0], 0);
+
+    // foreach (var path in Paths.Values.SelectMany(it => it).OrderBy(it => it)) Console.WriteLine(path);
+    result.Should().Be(expected);
+  }
 
   private readonly Dictionary<string, BigInteger> PathCache = [];
+  // private readonly Dictionary<string, List<string>> Paths = [];
   BigInteger ComputeNumberOfPaths(Staircase current, long step)
   {
+    var key = $"{current.Name}_{step}";
+    if (PathCache.TryGetValue(key, out var needle)) return needle;
+    // Paths[key] = Paths.GetValueOrDefault(key) ?? [];
     if (current.To == "END" && step == current.LastStep)
     {
+      PathCache[key] = 1;
+      // Paths[key].Add(key);
       return 1;
     }
-    var key = $"{current.Name}_{step}";
-
-    if (PathCache.TryGetValue(key, out var needle)) return needle;
 
     BigInteger total = 0;
+    HashSet<string> closed = [];
     foreach (var move in Moves)
     {
-      HashSet<string> closed = [];
       foreach (var (nextStaircase, nextStep) in Open(current, step, move))
       {
-        // var temp = ComputeNumberOfPaths(nextStaircase, nextStep);
-        // Console.WriteLine($"{current.Name} {step} + {move} -> {nextStaircase.Name} {nextStep} = {total + temp}");
+        if (!closed.Add($"{nextStaircase}_{nextStep}")) continue;
+        var temp = ComputeNumberOfPaths(nextStaircase, nextStep);
         total += ComputeNumberOfPaths(nextStaircase, nextStep);
+        // Paths[key].AddRange(Paths[$"{nextStaircase.Name}_{nextStep}"].Select(it => $"{key}->{it}"));
       }
     }
+    // Console.WriteLine($"{current.Name} {step} = {total}");
+
     PathCache[key] = total;
     return total;
   }
@@ -81,17 +92,17 @@ public class Problem21
     if (OpenCache.TryGetValue(key, out var needle)) return needle;
     if (step < current.LastStep)
     {
-      result = [..result, .. Open(current, step + 1, move - 1)];
+      result = [.. result, .. Open(current, step + 1, move - 1)];
     }
     if (step == current.LastStep && current.To != "END")
     {
-      result = [..result, .. Open(StaircaseMap[current.To], step, move - 1)];
+      result = [.. result, .. Open(StaircaseMap[current.To], step, move - 1)];
     }
     var neighbors = Staircases.Where(sc => sc.From == current.Name && sc.FirstStep == step).ToList();
     neighbors.Should().HaveCountLessThanOrEqualTo(1); // sanity
     foreach (var next in neighbors)
     {
-      result = [..result, .. Open(next, step, move - 1)];
+      result = [.. result, .. Open(next, step, move - 1)];
     }
     OpenCache[key] = result;
     return result;
