616779ef9a79b62549915536a64b62ff8a2566de
Jake Baker <SEP.Baker@fronius.com>
2025-08-18 12:10:56 -0400

    Codyssi.22.2.wip

--- Codyssi/2025/Problem22.cs
+++ Codyssi/2025/Problem22.cs
@@ -5,6 +5,7 @@ using Utils;
 using Mng.Quest.CSharp.Utils;
 using System.Numerics;
 using System.Data;
+using System.ComponentModel;
 
 namespace Mnq.Quest.CSharp.Codyssi;
 
@@ -20,25 +21,112 @@ public class Problem22
     input.SelectMany(it => Find(it, [maxX, maxY, maxZ])).Should().HaveCount(expected);
   }
 
-  public static IEnumerable<List<long>> Find(Rule r, List<long> maxXYZ)
+  [Theory]
+  [InlineData("Problem22.Sample.1.txt", 3, 3, 5, 23)]
+  // [InlineData("Problem22.Sample.2.txt", 10, 15, 60, 32545)]
+  // [InlineData("Problem22.txt", 10, 15, 60, 32422)]
+  public void Part2(string inputFile, long maxX, long maxY, long maxZ, int expected)
+  {
+    var input = GetInput(inputFile);
+    FindPath(input, maxX, maxY, maxZ).Should().Be(expected);
+  }
+
+  private static long FindPath(List<Rule> input, long maxX, long maxY, long maxZ)
+  {
+    var debris = input.SelectMany(it => Find(it, [maxX, maxY, maxZ])).ToList();
+
+    List<List<Debris>> debrisAtTimestamp = [];
+    List<HashSet<Point4>> occupiedAtTimestamp = [];
+    debrisAtTimestamp.Add([.. debris]);
+    occupiedAtTimestamp.Add([.. debris.Select(it => it.Position)]);
+
+    Point4 goal = new(maxX - 1, maxY - 1, maxZ - 1, 0);
+    
+    PriorityQueue<(Point4 Position, int Seconds)> open = new(a => a.Seconds + goal.ManhattanDistance(a.Position));
+
+    open.Enqueue((Point4.Zero, 0));
+    List<long> limits = [maxX, maxY, maxZ];
+
+    while (open.TryDequeue(out var current))
+    {
+      var nextTimestamp = current.Seconds + 1;
+      if (nextTimestamp >= debrisAtTimestamp.Count)
+      {
+        debrisAtTimestamp.Add(debrisAtTimestamp[^1].Select(a => a.Move(limits)).ToList());
+        occupiedAtTimestamp.Add([.. debrisAtTimestamp[^1].Select(it => it.Position)]);
+      }
+      foreach(var next in Neighbors(current.Position, occupiedAtTimestamp[nextTimestamp], limits))
+      {
+        if (next == goal) return nextTimestamp;
+      }
+    }
+
+    throw new ApplicationException();
+  }
+
+  static IEnumerable<Point4> Neighbors(Point4 position, HashSet<Point4> debris, List<long> limits)
+  {
+    foreach(var delta in new[]{ 
+      new Point4(1,0,0,0),
+      new Point4(-1,0,0,0),
+      new Point4(0,1,0,0),
+      new Point4(0,-1,0,0),
+      new Point4(0,0,1,0),
+      new Point4(0,0,-1,0),
+      new Point4(0,0,0,1),
+      new Point4(0,0,0,-1),
+      new Point4(0,0,0,0),
+    })
+    {
+      var next = position + delta;
+      if (next.X < 0 || next.X >= limits[0]) continue;
+      if (next.Y < 0 || next.Y >= limits[1]) continue;
+      if (next.Z < 0 || next.Z >= limits[2]) continue;
+      if (next.W < -1 || next.W > 1) continue;
+      if (next == Point4.Zero || !debris.Contains(next)) yield return next;
+    }
+  }
+
+  public static IEnumerable<Debris> Find(Rule r, List<long> maxXYZ)
   {
     for (long x = 0; x < maxXYZ[0]; x += 1)
     for (long y = 0; y < maxXYZ[1]; y += 1)
     for (long z = 0; z < maxXYZ[2]; z += 1)
     for (long w = -1; w <= 1; w += 1)
-    if (MathUtils.MathMod(x * r.Coefficients[0] + y * r.Coefficients[1] + z * r.Coefficients[2] + w * r.Coefficients[3],
-      r.Divisor) == r.Remainder) yield return [x, y, z, w];
+    if (MathUtils.MathMod(x * r.Coefficients.X + y * r.Coefficients.Y + z * r.Coefficients.Z + w * r.Coefficients.W,
+      r.Divisor) == r.Remainder) yield return new(new (x, y, z, w), r.Velocity);
   }
 
-  public record Rule(List<long> Coefficients, long Divisor, long Remainder, List<long> Velocity);
+  public record Point4(long X, long Y, long Z, long W)
+  {
+    public long ManhattanDistance(Point4 other) => Math.Abs(X - other.X) + Math.Abs(Y - other.Y) + Math.Abs(Z - other.Z) + Math.Abs(W - other.W);
+    public static Point4 operator +(Point4 lhs, Point4 rhs) => new(lhs.X + rhs.X, lhs.Y + rhs.Y, lhs.Z + rhs.Z, lhs.W + rhs.W);
+    public static Point4 Zero { get; } = new(0, 0, 0, 0);
+  }
 
-  private List<Rule> GetInput(string inputFile)
+  public record Debris(Point4 Position, Point4 Velocity)
+  {
+    internal Debris Move(List<long> limits)
+    {
+      var position = new Point4(
+        MathUtils.MathMod(Position.X + Velocity.X, limits[0]),
+        MathUtils.MathMod(Position.Y + Velocity.Y, limits[1]),
+        MathUtils.MathMod(Position.Z + Velocity.Z, limits[2]),
+        MathUtils.MathMod(Position.W + Velocity.W + 1, 3) - 1
+      );
+      return this with { Position =  position};
+    }
+  }
+
+  public record Rule(Point4 Coefficients, long Divisor, long Remainder, Point4 Velocity);
+
+  private static List<Rule> GetInput(string inputFile)
   {
     var p1 = P.Format("{}x+{}y+{}z+{}a", P.Long, P.Long, P.Long, P.Long)
-      .Select(it => new List<long> { it.First, it.Second, it.Third, it.Fourth });
+      .Select(it => new Point4(it.First, it.Second, it.Third, it.Fourth));
     return P.Format("RULE {}: {} DIVIDE {} HAS REMAINDER {} | DEBRIS VELOCITY ({})",
       P.Long, p1, P.Long, P.Long, P.Long.Star(","))
-      .Select(it => new Rule(it.Second, it.Third, it.Fourth, it.Fifth))
+      .Select(it => new Rule(it.Second, it.Third, it.Fourth, new Point4(it.Fifth[0], it.Fifth[1], it.Fifth[2], it.Fifth[3])))
       .ParseMany(CodyssiLoader.ReadLines(inputFile));
 
   }
