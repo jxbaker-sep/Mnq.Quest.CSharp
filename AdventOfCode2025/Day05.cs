e890f6f25e3a931ddfb161e03bc11f13df57a0d9
Jake Baker <jxbaker@sep.com>
2025-12-05 06:08:46 -0500

    day05

---
+++ AdventOfCode2025/Day05.cs
@@ -0,0 +1,77 @@
+
+using FluentAssertions;
+using P = Parser.ParserBuiltins;
+using Parser;
+using Mng.Quest.CSharp.Utils;
+
+namespace Mng.Quest.CSharp.AdventOfCode2025;
+
+public class Day05
+{
+
+
+  [Theory]
+  [InlineData("Day05.Sample.txt", 3)]
+  [InlineData("Day05.txt", 885)]
+  public void Part1(string inputFile, long expected)
+  {
+    var pps = AdventOfCode2025Loader.ReadAllText(inputFile).Paragraphs();
+
+    var ranges = P.Format("{}-{}", P.Long, P.Long).End().ParseMany(pps[0]);
+    var ingredients = P.Long.End().ParseMany(pps[1]);
+
+    ingredients.Where(it => ranges.Any(r => r.First <= it && it <= r.Second))
+      .Count().Should().Be((int)expected);
+  }
+
+  [Theory]
+  [InlineData("Day05.Sample.txt", 14)]
+  [InlineData("Day05.txt", 348115621205535)]
+  public void Part2(string inputFile, long expected)
+  {
+    var pps = AdventOfCode2025Loader.ReadAllText(inputFile).Paragraphs();
+
+    var ranges = P.Format("{}-{}", P.Long, P.Long).End().ParseMany(pps[0]);
+
+    Queue<(long first, long second)> open = new([.. ranges]);
+    LinkedList<(long first, long second)> closed = [];
+
+    while (open.TryDequeue(out var current))
+    {
+      bool anyCombined = false;
+      var first = closed.First;
+      while (first != null)
+      {
+        var combined = TryCombineRanges(current, first.Value);
+        if (combined is {} c)
+        {
+          var temp = first;
+          first = first.Next;
+          open.Enqueue(c);
+          closed.Remove(temp);
+          anyCombined = true;
+          continue;
+        }
+        first = first.Next;
+      }
+      if (!anyCombined) closed.AddLast(current);
+    }
+
+
+    closed.Sum(it => it.second - it.first + 1).Should().Be(expected);
+  }
+
+  (long first, long second)? TryCombineRanges((long first, long second) one, (long first, long second) two)
+  {
+    if ((two.first <= one.first && one.first <= two.second) ||
+        (two.first <= one.second && one.second <= two.second) ||
+        (one.first <= two.first && two.first <= one.second) ||
+        (one.first <= two.second && two.second <= one.second)
+    )
+    {
+      return (Math.Min(one.first, two.first), Math.Max(one.second, two.second));
+    }
+    return null;
+  }
+
+}
