7cc6f4a276b90f87f74eb42f82d4bcbe8ebdf2b8
Jake Baker <jxbaker@sep.com>
2025-12-08 07:11:15 -0500

    day08.1

---
+++ AdventOfCode2025/Day08.cs
@@ -0,0 +1,52 @@
+
+using FluentAssertions;
+using P = Parser.ParserBuiltins;
+using Parser;
+using Mng.Quest.CSharp.Utils;
+using Utils;
+
+namespace Mng.Quest.CSharp.AdventOfCode2025;
+
+public class Day08
+{
+  [Theory]
+  [InlineData("Day08.Sample.txt", 10, 40)]
+  [InlineData("Day08.txt", 1_000, 102816)]
+  public void Part1(string inputFile, long connections, long expected)
+  {
+    var grid = P.Long.Star(",").End().Select(it => new Point3(it[0], it[1], it[2]))
+      .ParseMany(AdventOfCode2025Loader.ReadLines(inputFile));
+
+    Dictionary<Point3, DisjointSet> sets = [];
+
+    List<(Point3, Point3)> distances = [];
+
+    foreach (var (p1, index) in grid.WithIndices())
+    {
+      foreach (var p2 in grid[(index + 1)..])
+      {
+        distances.Add((p1, p2));
+      }
+    }
+
+    distances = distances.OrderBy(it => it.Item1.StraightLineDistance(it.Item2)).ToList();
+
+    for (var i = 0; i < connections; i++)
+    {
+      var (p1, p2) = distances[i];
+      var dj1 = sets.GetValueOrDefault(p1, new());
+      var dj2 = sets.GetValueOrDefault(p2, new());
+      dj1.Union(dj2);
+      sets[p1] = dj1.Find();
+      sets[p2] = sets[p1];
+    }
+
+    var zed = sets.Values.Select(it => it.Find())
+      .GroupToCounts()
+      .Select(it => (long)it.Value)
+      .OrderByDescending(it => it)
+      .ToList();
+    zed.Take(3).Product()
+    .Should().Be(expected);
+  }
+}
--- Utils/Point3.cs
+++ Utils/Point3.cs
@@ -8,4 +8,10 @@ public static class Point3Extensions
   public static Point3 RotateLeft(this Point3 self) => new(-self.Y, self.X, self.Z);
   public static Point3 RotateUp(this Point3 self) => new(self.X, -self.Z, self.Y);
   public static Point3 RotateDown(this Point3 self) => new(self.X, self.Z, -self.Y);
+
+  public static long ManhattanDistance(this Point3 self, Point3 other) =>
+    Math.Abs(self.X - other.X) + Math.Abs(self.Y - other.Y) + Math.Abs(self.Z - other.Z);
+
+  public static double StraightLineDistance(this Point3 self, Point3 other) =>
+    Math.Sqrt(Math.Pow(self.X - other.X, 2) + Math.Pow(self.Y - other.Y, 2) + Math.Pow(self.Z - other.Z, 2));
 }
