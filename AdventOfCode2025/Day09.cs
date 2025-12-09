c0646a49e771f2ee291bb92341fb3a52f66de952
Jake Baker <jxbaker@sep.com>
2025-12-09 12:25:08 -0500

    day09.yay

--- AdventOfCode2025/Day09.cs
+++ AdventOfCode2025/Day09.cs
@@ -27,7 +27,8 @@ public class Day09
 
   [Theory]
   [InlineData("Day09.Sample.txt", 24)]
-  [InlineData("Day09.txt", 0)] // 4634026886 too high
+  [InlineData("Day09.txt", 1396494456)] // 4634026886 too high, 443075248 too low
+                              //  1396494456
   public void Part2(string inputFile, long expected)
   {
     var points = P.Long.Star(",").End().Select(it => new Point(it[0], it[1]))
@@ -36,6 +37,7 @@ public class Day09
     var vlines = points.Windows(2).Append([points[0], points[^1]])
       .Where(it => it[0].X == it[1].X)
       .Select(it => (top: Math.Min(it[0].Y, it[1].Y), bottom: Math.Max(it[0].Y, it[1].Y), x: it[0].X))
+      .OrderBy(it => it.x)
       .ToList();
 
     var hlines = points.Windows(2).Append([points[0], points[^1]])
@@ -45,39 +47,92 @@ public class Day09
 
     var result = -1L;
 
-    foreach (var pair in points.Pairs())
+    var pairs = points.Pairs().ToList();
+    var i = 0;
+    foreach (var (First, Second) in pairs)
     {
-      var minx = Math.Min(pair.First.X, pair.Second.X);
-      var maxx = Math.Max(pair.First.X, pair.Second.X);
-      var miny = Math.Min(pair.First.Y, pair.Second.Y);
-      var maxy = Math.Max(pair.First.Y, pair.Second.Y);
-      var found = true;
-      foreach (var vertex in new[] { new Point(minx, miny), new Point(minx, maxy), new Point(maxx, miny), new Point(maxx, maxy) })
+      if ((i++) % 10 == 0)
+        Console.WriteLine($"{i} / {pairs.Count}");
+      var minx = Math.Min(First.X, Second.X);
+      var maxx = Math.Max(First.X, Second.X);
+      var miny = Math.Min(First.Y, Second.Y);
+      var maxy = Math.Max(First.Y, Second.Y);
+      var upperLeft = new Point(minx, miny);
+      var upperRight = new Point(maxx, miny);
+      var lowerLeft = new Point(minx, maxy);
+      var lowerRight = new Point(maxx, maxy);
+      // var found = true;
+      var area = (Math.Abs(First.X - Second.X) + 1) * (Math.Abs(First.Y - Second.Y) + 1);
+      if (area == 4634026886)
       {
-        if (!PointInPolygon(vertex, vlines, hlines))
-        {
-          found = false;
-          break;
-        }
+        Console.WriteLine("");
       }
-      if (found) {
-        result = Math.Max(result, (Math.Abs(pair.First.X - pair.Second.X) + 1) * (Math.Abs(pair.First.Y - pair.Second.Y) + 1));
+      var found = LineInPolygon(upperLeft, upperRight, vlines, hlines) && LineInPolygon(upperRight, lowerRight, vlines, hlines) && LineInPolygon(lowerLeft, lowerRight, vlines, hlines) && LineInPolygon(lowerLeft, upperLeft, vlines, hlines);
+      if (found)
+      {
+        result = Math.Max(result, area);
       }
     }
 
     result.Should().Be(expected);
   }
 
+  private bool LineInPolygon(Point First, Point Second, List<(long top, long bottom, long x)> vlines, List<(long left, long right, long y)> hlines)
+  {
+    var minx = Math.Min(First.X, Second.X);
+    var maxx = Math.Max(First.X, Second.X);
+    var miny = Math.Min(First.Y, Second.Y);
+    var maxy = Math.Max(First.Y, Second.Y);
+    if (!PointInPolygon(new(minx, miny), vlines, hlines) || !PointInPolygon(new(minx, maxy), vlines, hlines) || !PointInPolygon(new(maxx, miny), vlines, hlines) || !PointInPolygon(new(maxx, maxy), vlines, hlines)) return false;
+
+
+    if (AnyLinesIntersect(vlines, hlines, minx, maxx, miny, maxy)) return false;
+
+    return true;
+  }
+
+  private static bool AnyLinesIntersect(List<(long top, long bottom, long x)> vlines, List<(long left, long right, long y)> hlines, long minx, long maxx, long miny, long maxy)
+  {
+    if (minx > maxx || miny > maxy) return false;
+    if (vlines.Any(line => line.x > minx && line.x < maxx && line.top < miny && miny < line.bottom)) return true;
+    if (vlines.Any(line => line.x > minx && line.x < maxx && line.top < maxy && maxy < line.bottom)) return true;
+    if (hlines.Any(line => line.y > miny && line.y < maxy && line.left < minx && minx < line.right)) return true;
+    if (hlines.Any(line => line.y > miny && line.y < maxy && line.left < maxx && maxx < line.right)) return true;
+    return false;
+  }
+
+  private static IEnumerable<Point> PointsBetween(Point First, Point Second)
+  {
+    var minx = First.X;
+    var maxx = First.X;
+    var miny = First.Y;
+    var maxy = First.Y;
+    if (First.X == Second.X)
+    {
+      miny = Math.Min(First.Y, Second.Y);
+      maxy = Math.Max(First.Y, Second.Y);
+    }
+    else if (First.Y == Second.Y)
+    {
+      minx = Math.Min(First.X, Second.X);
+      maxx = Math.Max(First.X, Second.X);
+    }
+    else throw new ApplicationException();
+    for (var x = minx; x <= maxx; x++)
+      for (var y = miny; y <= maxy; y++)
+        yield return new(x, y);
+  }
+
   private static bool PointInPolygon(Point vertex, List<(long top, long bottom, long x)> vlines, List<(long left, long right, long y)> hlines)
   {
-    foreach(var (left, right, y) in hlines) 
+    foreach (var (left, right, y) in hlines)
       if (y == vertex.Y && left <= vertex.X && vertex.X <= right)
         return true;
 
-    foreach(var (top, bottom, x) in vlines) 
+    foreach (var (top, bottom, x) in vlines)
       if (x == vertex.X && top <= vertex.Y && vertex.Y <= bottom)
         return true;
 
-    return vlines.Count(line => line.x <= vertex.X && line.top <= vertex.Y && vertex.Y < line.bottom) % 2 == 1;
+    return vlines.TakeWhile(line => line.x <= vertex.X).Count(line => line.top <= vertex.Y && vertex.Y < line.bottom) % 2 == 1;
   }
 }
