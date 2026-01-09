895405fc4b7e2f8db18e5724406352d91e7afbe5
Jake Baker <jxbaker@sep.com>
2026-01-09 11:55:52 -0500

    ec2024.q13.3

--- EverybodyCodes/Q2024/Quest13.cs
+++ EverybodyCodes/Q2024/Quest13.cs
@@ -14,15 +14,28 @@ public class Quest13
   {
     var world = GetInput(inputFile);
 
-    FindPath(world).Should().Be(expected);
+    FindPath(world, world.Starts.Single()).Should().Be(expected);
   }
 
-  private long FindPath(World world)
+  [Theory]
+  // [InlineData("Quest13.3.Sample.txt", 14)]
+  [InlineData("Quest13.3.txt", 529)]
+  public void Part3(string inputFile, long expected)
   {
-    var (grid, start, goal) = world;
+    var world = GetInput(inputFile);
 
-    PriorityQueue<(Point position, HashSet<Point> closed, long steps)> open = new(it => it.steps);
-    open.Enqueue((start, [start], 0));
+    // world.Starts.Min(start => FindPath(world, start)).Should().Be(expected);
+    ReverseFindPath(world).Should().Be(expected);
+
+  }
+
+  private long FindPath(World world, Point start)
+  {
+    var grid = world.Grid;
+    var goal = world.Goal;
+
+    PriorityQueue<(Point position, long steps)> open = new(it => it.steps);
+    open.Enqueue((start, 0));
     Dictionary<Point, long> closed = [];
     closed[start] = 0;
 
@@ -30,12 +43,48 @@ public class Quest13
     {
       foreach (var neighbor in Open(grid, current.position))
       {
-        if (current.closed.Contains(neighbor)) continue;
         var d = current.steps + 1 + Elevate(grid[current.position], grid[neighbor]);
         if (closed.GetValueOrDefault(neighbor, long.MaxValue) < d) continue;
         closed[neighbor] = d;
         if (neighbor == goal) return d;
-        open.Enqueue((neighbor, [..current.closed, neighbor], d));
+        open.Enqueue((neighbor, d));
+      }
+    }
+
+    throw new ApplicationException();
+  }
+
+  private long ReverseFindPath(World world)
+  {
+    var grid = world.Grid;
+    var start = world.Goal;
+    var goal = world.Starts;
+
+    PriorityQueue<(Point position, List<Point> path, long steps)> open = new(it => it.steps);
+    open.Enqueue((start, [start], 0));
+    Dictionary<Point, long> closed = [];
+    closed[start] = 0;
+    var min = long.MaxValue;
+
+    while (open.TryDequeue(out var current))
+    {
+      if (goal.Contains(current.position))
+      {
+        return current.steps;
+      }
+      if (current.steps >= min) continue;
+      foreach (var neighbor in Open(grid, current.position))
+      {
+        if (current.path.Contains(neighbor)) continue;
+        var d = current.steps + 1 + Elevate(grid[current.position], grid[neighbor]);
+        if (d > min) continue;
+        if (closed.GetValueOrDefault(neighbor, long.MaxValue) < d) continue;
+        closed[neighbor] = d;
+        if (goal.Contains(current.position))
+        {
+          min = Math.Min(min, d);
+        }
+        open.Enqueue((neighbor, [.. current.path, neighbor], d));
       }
     }
 
@@ -44,12 +93,12 @@ public class Quest13
 
   private static long Elevate(int from, int to)
   {
-    return new[]{Math.Abs(from - to), from + (10 - to), (10 - from) + to}.Min();
+    return new[] { Math.Abs(from - to), from + (10 - to), (10 - from) + to }.Min();
   }
 
   private IEnumerable<Point> Open(Grid<int> grid, Point position)
   {
-    foreach(var neighbor in position.CardinalNeighbors())
+    foreach (var neighbor in position.CardinalNeighbors())
     {
       if (grid.Get(neighbor, Wall) == Wall) continue;
       yield return neighbor;
@@ -57,13 +106,13 @@ public class Quest13
   }
 
   const int Wall = -1;
-  record World(Grid<int> Grid, Point Start, Point Goal);
+  record World(Grid<int> Grid, List<Point> Starts, Point Goal);
   private static World GetInput(string inputFile)
   {
     var temp = ECLoader.ReadLines(inputFile).Gridify();
-    var grid = ECLoader.ReadLines(inputFile).Gridify(it => it switch{'#' => Wall, ' ' => Wall, 'S' => 0, 'E' => 0,  _ => it - '0'});
+    var grid = ECLoader.ReadLines(inputFile).Gridify(it => it switch { '#' => Wall, ' ' => Wall, 'S' => 0, 'E' => 0, _ => it - '0' });
 
-    return new(grid, temp.Items().Single(it => it.Value == 'S').Key, temp.Items().Single(it => it.Value == 'E').Key);
+    return new(grid, [.. temp.Items().Where(it => it.Value == 'S').Select(it => it.Key)], temp.Items().Single(it => it.Value == 'E').Key);
   }
 
 }
