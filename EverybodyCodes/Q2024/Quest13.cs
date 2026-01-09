2656df34f7810cfebc74671c292d25cda7fceb4c
Jake Baker <jxbaker@sep.com>
2026-01-09 12:26:52 -0500

    ec2024.q13.so-much-faster

--- EverybodyCodes/Q2024/Quest13.cs
+++ EverybodyCodes/Q2024/Quest13.cs
@@ -6,62 +6,92 @@ namespace Mng.Quest.CSharp.EverybodyCodes.Q2024;
 
 public class Quest13
 {
+  // [Theory]
+  // [InlineData("Quest13.1.Sample.txt", 28)]
+  // [InlineData("Quest13.1.txt", 159)]
+  // [InlineData("Quest13.2.txt", 646)]
+  // [InlineData("Quest13.3.Sample.txt", 14)]
+  // [InlineData("Quest13.3.txt", 529)]
+  // public void Part1(string inputFile, long expected)
+  // {
+  //   var world = GetInput(inputFile);
+
+  //   FindPath(world).Should().Be(expected);
+  // }
+
   [Theory]
   [InlineData("Quest13.1.Sample.txt", 28)]
   [InlineData("Quest13.1.txt", 159)]
   [InlineData("Quest13.2.txt", 646)]
   [InlineData("Quest13.3.Sample.txt", 14)]
   [InlineData("Quest13.3.txt", 529)]
-  public void Part1(string inputFile, long expected)
+  public void Painterly(string inputFile, long expected)
   {
     var world = GetInput(inputFile);
 
-    FindPath(world).Should().Be(expected);
+    FindPainterlyPath(world).Should().Be(expected);
   }
 
-  private long FindPath(World world)
+  private static long FindPainterlyPath(World world)
   {
     var grid = world.Grid;
     var start = world.Goal;
     var goal = world.Starts.ToHashSet();
 
-    PriorityQueue<(Point position, long steps)> open = new(it => it.steps);
-    open.Enqueue((start, 0));
+    Queue<Point> open = new([start]);
     Dictionary<Point, long> closed = [];
     closed[start] = 0;
-    var min = long.MaxValue;
 
     while (open.TryDequeue(out var current))
     {
-      if (goal.Contains(current.position))
+      var currentSteps = closed[current];
+      foreach (var neighbor in Open(grid, current))
       {
-        return current.steps;
-      }
-      if (current.steps >= min) continue;
-      foreach (var neighbor in Open(grid, current.position))
-      {
-        var d = current.steps + 1 + Elevate(grid[current.position], grid[neighbor]);
-        if (d > min) continue;
-        if (closed.GetValueOrDefault(neighbor, long.MaxValue) < d) continue;
+        var d = currentSteps + 1 + Elevate(grid[current], grid[neighbor]);
+        if (closed.GetValueOrDefault(neighbor, long.MaxValue) <= d) continue;
         closed[neighbor] = d;
-        if (goal.Contains(neighbor))
-        {
-          if (d < min) min = d;
-          else continue;
-        }
-        open.Enqueue((neighbor, d));
+        open.Enqueue(neighbor);
       }
     }
 
-    throw new ApplicationException();
+    return goal.Min(g => closed[g]);
   }
+
+  // private static long FindPath(World world)
+  // {
+  //   var grid = world.Grid;
+  //   var start = world.Goal;
+  //   var goal = world.Starts.ToHashSet();
+
+  //   PriorityQueue<(Point position, long steps)> open = new(it => it.steps);
+  //   open.Enqueue((start, 0));
+  //   Dictionary<Point, long> closed = [];
+  //   closed[start] = 0;
+
+  //   while (open.TryDequeue(out var current))
+  //   {
+  //     if (goal.Contains(current.position))
+  //     {
+  //       return current.steps;
+  //     }
+  //     foreach (var neighbor in Open(grid, current.position))
+  //     {
+  //       var d = current.steps + 1 + Elevate(grid[current.position], grid[neighbor]);
+  //       if (closed.GetValueOrDefault(neighbor, long.MaxValue) < d) continue;
+  //       closed[neighbor] = d;
+  //       open.Enqueue((neighbor, d));
+  //     }
+  //   }
+
+  //   throw new ApplicationException();
+  // }
 
   private static long Elevate(int from, int to)
   {
     return new[] { Math.Abs(from - to), from + (10 - to), (10 - from) + to }.Min();
   }
 
-  private IEnumerable<Point> Open(Grid<int> grid, Point position)
+  private static IEnumerable<Point> Open(Grid<int> grid, Point position)
   {
     foreach (var neighbor in position.CardinalNeighbors())
     {
