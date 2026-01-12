a158b2559113f6da33327a1cb8b9c105308613ea
Jake Baker <jxbaker@sep.com>
2026-01-12 10:59:36 -0500

    ec2024.q14.2

--- EverybodyCodes/Q2024/Quest14.cs
+++ EverybodyCodes/Q2024/Quest14.cs
@@ -11,37 +11,65 @@ public class Quest14
   [InlineData("Quest14.1.txt", 144)]
   public void Part1(string inputFile, long expected)
   {
-    var steps = GetInput(inputFile);
+    var lines = GetInput(inputFile);
+    Tree(lines.Single()).Max(it => it.Z).Should().Be(expected);
+  }
 
+  [Theory]
+  [InlineData("Quest14.2.Sample.txt", 32)]
+  [InlineData("Quest14.2.txt", 5074)]
+  public void Part2(string inputFile, int expected)
+  {
+    var lines = GetInput(inputFile);
+    Dictionary<Point3, long> overlaps = [];
+    foreach(var steps in lines)
+    {
+      foreach(var p in Tree(steps))
+      {
+        overlaps[p] = overlaps.GetValueOrDefault(p) + 1;
+      }
+    }
+    overlaps.Count.Should().Be(expected);
+  }
+
+  HashSet<Point3> Tree(List<Step> steps)
+  {
+    HashSet<Point3> result = [];
     var current = Point3.Zero;
-    long highWaterMark = 0;
     foreach(var step in steps)
     {
-      current += step.V * step.Scalar;
-      highWaterMark = Math.Max(highWaterMark, current.Z);
+      for(long i = 0; i < step.Scalar; i++)
+      {
+        current += step.V;
+        result.Add(current);
+      }
     }
-
-    highWaterMark.Should().Be(expected);
+    return result;
   }
 
   public record Step(Vector3 V, long Scalar);
 
-  private static List<Step> GetInput(string inputFile)
+  private static List<List<Step>> GetInput(string inputFile)
   {
-    List<Step> result = [];
-    var temp = ECLoader.ReadLines(inputFile).Single();
-    foreach (var step in temp.Split(","))
+    List<List<Step>> result = [];
+    var lines = ECLoader.ReadLines(inputFile);
+    foreach (var line in lines)
     {
-      result.Add(new(step[0] switch
+      List<Step> temp = [];
+      result.Add(temp);
+      foreach (var step in line.Split(","))
       {
-        'U' => Vector3.Up,
-        'D' => Vector3.Down,
-        'R' => Vector3.East,
-        'L' => Vector3.West,
-        'F' => Vector3.North,
-        'B' => Vector3.South,
-        _ => throw new ApplicationException()
-      }, Convert.ToInt64(step[1..])));
+        temp.Add(new(step[0] switch
+        {
+          'U' => Vector3.Up,
+          'D' => Vector3.Down,
+          'R' => Vector3.East,
+          'L' => Vector3.West,
+          'F' => Vector3.North,
+          'B' => Vector3.South,
+          _ => throw new ApplicationException()
+        }, Convert.ToInt64(step[1..])));
+      }
     }
     return result;
   }
