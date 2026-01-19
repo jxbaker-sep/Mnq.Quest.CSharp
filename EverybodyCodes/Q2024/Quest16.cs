b09aea55e635b5c17419fa51b8e6b286a7aa55be
Jake Baker <jxbaker@sep.com>
2026-01-19 16:02:51 -0500

    16.2

--- EverybodyCodes/Q2024/Quest16.cs
+++ EverybodyCodes/Q2024/Quest16.cs
@@ -17,13 +17,42 @@ public class Quest16
   {
     var world = GetInput(inputFile);
 
-    world.PullSize.Zip(world.Wheels, (ps, wheel) => (ps, wheel))
+    world.PullSizes.Zip(world.Wheels, (ps, wheel) => (ps, wheel))
       .Select(it => it.wheel[(100 * it.ps) % it.wheel.Count])
       .Join(" ")
       .Should().Be(expected);
   }
 
-  private record World(List<int> PullSize, List<List<string>> Wheels);
+  [Theory]
+  [InlineData("Quest16.2.Sample.txt", 280014668134)]
+  [InlineData("Quest16.2.txt", 114778950421)]
+  public void Part2(string inputFile, long expected)
+  {
+    var (pullSizes, wheels) = GetInput(inputFile);
+    var current = pullSizes.Select(_ => 0).ToList();
+
+    List<long> pullToScore = [];
+
+    for(var i = 0 ; true; i++)
+    {
+      foreach(var (pullsize, index) in pullSizes.WithIndices()) current[index] = (current[index] + pullsize) % wheels[index].Count;
+      var asString = current.Zip(wheels).Select(it => it.Second[it.First][0..1] + it.Second[it.First][2..3]).Join();
+      var faces = asString.GroupToCounts();
+      pullToScore.Add(faces.Values.Where(it => it >= 3).Sum(it => it - 2));
+      if (current.All(it => it == 0)) break;
+    }
+
+    const long pulls = 202420242024;
+
+    long cycles = pulls / pullToScore.Count;
+    long last = cycles * pullToScore.Count;
+    (pulls - last).Should().BeLessThan(pullToScore.Count);
+
+    var result = pullToScore.Sum() * cycles + pullToScore.Take((int)(pulls - last)).Sum();
+    result.Should().Be(expected);
+  }
+
+  private record World(List<int> PullSizes, List<List<string>> Wheels);
 
   private static World GetInput(string inputFile)
   {
