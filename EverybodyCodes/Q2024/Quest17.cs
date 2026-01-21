7c9c48fded0fd28c3af2bdac23aef237724c47e5
Jake Baker <jxbaker@sep.com>
2026-01-21 10:47:13 -0500

    ec.25.17.3

--- EverybodyCodes/Q2024/Quest17.cs
+++ EverybodyCodes/Q2024/Quest17.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using FluentAssertions;
 using MathNet.Numerics;
 using Mng.Quest.CSharp.Utils;
@@ -19,29 +20,38 @@ public class Quest17
   {
     var stars = GetInput(inputFile);
 
-    (StarSearch(stars, long.MaxValue).First() + stars.Count).Should().Be(expected);
+    StarSearch(stars, long.MaxValue).First().Should().Be(expected);
   }
 
   [Theory]
   [InlineData("Quest17.3.Sample.txt", 15624)]
-  // [InlineData("Quest17.3.txt", 0)]
+  [InlineData("Quest17.3.txt", 4260330828)] // ~1315ms
   public void Part3(string inputFile, long expected)
   {
+    Stopwatch sw = new();
+    sw.Start();
     var stars = GetInput(inputFile);
 
+    var temp = StarSearch(stars, 6).ToList();
+
     StarSearch(stars, 6).OrderByDescending(it => it).Take(3).Product().Should().Be(expected);
+    Console.WriteLine(sw.ElapsedMilliseconds);
   }
 
   private static IEnumerable<long> StarSearch(Stars stars, long maxDistance)
   {
-    List<(Point Star1, Point Star2, long Distance)> mds = [];
+    List<(Point Star1, Point Star2, long Distance)> mdsTemp = [];
     for (var i = 0; i < stars.Count - 1; i++)
     {
       for (var j = i + 1; j < stars.Count; j++)
       {
-        mds.Add((stars[i], stars[j], stars[i].ManhattanDistance(stars[j])));
+        mdsTemp.Add((stars[i], stars[j], stars[i].ManhattanDistance(stars[j])));
       }
     }
+
+    mdsTemp.Sort((a, b) => a.Distance.CompareTo(b.Distance));
+
+    LinkedList<(Point Star1, Point Star2, long Distance)> ll = new(mdsTemp);
 
     long total = 0;
     HashSet<Point> constellation = [];
@@ -49,26 +59,45 @@ public class Quest17
 
     while (available.Count > 0)
     {
-      var (Star1, Star2, Distance) = mds
-        .Where(it => available.Contains(it.Star1) || available.Contains(it.Star2))
-        .Where(it => constellation.Count == 0 || constellation.Contains(it.Star1) || constellation.Contains(it.Star2))
-        .MinBy(it => it.Distance);
-
-      if (Distance >= maxDistance || constellation.Any(it => it.ManhattanDistance(Star1) >= maxDistance || it.ManhattanDistance(Star2) >= maxDistance))
+      (Point Star1, Point Star2, long Distance) found;
+      if (constellation.Count == 0)
       {
-        yield return total;
-        total = 0;
-        constellation.Clear();
+        var node = ll.Nodes()
+                   .First(it => available.Contains(it.Value.Star1) && available.Contains(it.Value.Star2));
+        if (node.Value.Distance >= maxDistance)
+        {
+          // items in available are all >= MaxDistance apart; we could return single-star constellations
+          // but we don't need to for this problem.
+          yield break;
+        }
+        ll.Remove(node);
+        found = node.Value;
       }
-      
-      constellation.Add(Star1);
-      constellation.Add(Star2);
-      available.Remove(Star1);
-      available.Remove(Star2);
-      total += Distance;
+      else
+      {
+        var nodes = ll.Nodes()
+                   .Where(it => available.Contains(it.Value.Star1) != available.Contains(it.Value.Star2))
+                   .TakeWhile(it => it.Value.Distance < maxDistance)
+                   .Take(1).ToList();
+        if (nodes.Count == 0)
+        {
+          yield return total + constellation.Count;
+          constellation.Clear();
+          total = 0;
+          continue;
+        }
+        ll.Remove(nodes[0]);
+        found = nodes[0].Value;
+      }
+
+      constellation.Add(found.Star1);
+      constellation.Add(found.Star2);
+      available.Remove(found.Star1);
+      available.Remove(found.Star2);
+      total += found.Distance;
     }
 
-    if (total > 0) yield return total;
+    if (total > 0) yield return total + constellation.Count;
   }
 
 
