4f2df7b0a57ac2a1c26464bb2408597e68472aea
Jake Baker <jxbaker@sep.com>
2026-01-23 11:10:09 -0500

    ec.25.18.3

--- EverybodyCodes/Q2024/Quest18.cs
+++ EverybodyCodes/Q2024/Quest18.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using FluentAssertions;
 using Mng.Quest.CSharp.Utils;
 using Mnq.Quest.CSharp.EverybodyCodes;
@@ -20,35 +21,73 @@ public class Quest18
   {
     var grid = GetInput(inputFile);
 
-    Paint(grid).Should().Be(expected);
+    List<Point> firsts = [.. grid.Points((point, value) => value == Open && (point.X == 0 || point.X == grid.Width - 1))];
+
+    Paint(grid, firsts).Should().Be(expected);
   }
 
-  private long Paint(Grid<char> grid)
+  [Theory]
+  [InlineData("Quest18.3.Sample.txt", 12)]
+  [InlineData("Quest18.3.txt", 248_879)] // 65.3s
+  public void Part3(string inputFile, long expected)
   {
-    var firsts = grid.Points((point, value) => value == Open && (point.X == 0 || point.X == grid.Width - 1 ));
+    var grid = GetInput(inputFile);
 
-    var unwatered = grid.Points((_, value) => value == 'P').ToHashSet();
+    long min = long.MaxValue;
+    List<Point> firsts = [.. grid.Points((point, value) => value == Open)];
+    var i = 0;
+    Stopwatch sw = new();
+    sw.Start();
+    foreach (var first in firsts)
+    {
+      i += 1;
+      var item = Paint(grid, [first], min, true);
+      Console.WriteLine($"{i}/{firsts.Count} {sw.ElapsedMilliseconds} {firsts.Count * sw.ElapsedMilliseconds / i}");
+      min = Math.Min(min, item);
+    }
+
+    min.Should().Be(expected);
+  }
+
+  private static long Paint(Grid<char> grid, List<Point> firsts, long maxValue = long.MaxValue, bool wantSum = false)
+  {
+    var allPlants = grid.Points((_, value) => value == 'P').ToHashSet();
     Dictionary<Point, long> watered = [];
 
     Dictionary<Point, long> closed = firsts.ToDictionary(it => it, _ => 0L);
 
     Queue<Point> open = new(firsts);
+    var remaining = allPlants.Count;
+    long currentSum = 0;
 
-    while (watered.Count < unwatered.Count && open.TryDequeue(out var current))
+    while (watered.Count < allPlants.Count && open.TryDequeue(out var current))
     {
       var d = closed[current];
-      foreach(var neighbor in current.CardinalNeighbors())
+      foreach (var neighbor in current.CardinalNeighbors())
       {
         var nv = grid.Get(neighbor, Wall);
         if (nv == Wall) continue;
         if (closed.TryGetValue(neighbor, out var existing) && existing <= d + 1) continue;
-        if (nv == PalmTree) watered[neighbor] = d + 1;
+        if (nv == PalmTree)
+        {
+          if (!watered.ContainsKey(neighbor)) remaining -= 1;
+          watered[neighbor] = d + 1;
+          currentSum = watered.Values.Sum();
+          if (remaining == 0)
+          {
+            if (!wantSum) return watered.Values.Max();
+            return watered.Values.Sum();
+          }
+        }
         closed[neighbor] = d + 1;
         open.Enqueue(neighbor);
+        var estimatedSum = currentSum + (d + 2) * remaining;
+          ;
+        if (estimatedSum >= maxValue) return maxValue;
       }
     }
 
-    return watered.Values.Max();
+    throw new ApplicationException();
   }
 
   private static Grid<char> GetInput(string inputFile)
