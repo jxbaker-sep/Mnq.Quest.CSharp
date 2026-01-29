952dcd3001db9814b72be4f019444d47d181875f
Jake Baker <jxbaker@sep.com>
2026-01-29 11:47:52 -0500

    ec.25.20.2.wip

--- EverybodyCodes/Q2024/Quest20.cs
+++ EverybodyCodes/Q2024/Quest20.cs
@@ -57,6 +57,75 @@ public partial class Quest20
     closed.Where(it => it.Key.Seconds == goalSeconds).Max(it => it.Value).Should().Be(expected);
   }
 
+  [Theory]
+  [InlineData("Quest20.2.Sample.1.txt", 24)]
+  // [InlineData("Quest20.2.Sample.2.txt", 78)]
+  // [InlineData("Quest20.2.Sample.3.txt", 206)]
+  // [InlineData("Quest20.2.txt", 0)]
+  public void Part2(string inputFile, long expected)
+  {
+    const long startingAltitude = 10_000;
+
+    var grid = GetInput(inputFile);
+    var S = grid.Points((_, value) => value == 'S').First();
+    var A = grid.Points((_, value) => value == 'A').First();
+    var B = grid.Points((_, value) => value == 'B').First();
+    var C = grid.Points((_, value) => value == 'C').First();
+    List<Point> checkpoints = [A, B, C, S];
+    Dictionary<Point, long> minimalRemainders = [];
+    minimalRemainders[S] = 0;
+    minimalRemainders[C] = C.ManhattanDistance(S);
+    minimalRemainders[B] = B.ManhattanDistance(C) + minimalRemainders[C];
+    minimalRemainders[A] = A.ManhattanDistance(B) + minimalRemainders[B];
+
+    PriorityQueue<(Point Position, long Seconds, Vector Vector, Point Want, long Altitude)> open = new(current =>
+    {
+      return current.Seconds +  minimalRemainders[current.Want] + current.Position.ManhattanDistance(current.Want);
+    });
+    open.Enqueue((S, 0, Vector.South, A, startingAltitude));
+
+    while (open.TryDequeue(out var current))
+    {
+      foreach(var nextVector in new[]{current.Vector, current.Vector.RotateLeft(), current.Vector.RotateRight()})
+      {
+        var neighbor = current.Position + nextVector;
+        var nextWant = current.Want;
+        var currentAltitude = current.Altitude;
+        Console.WriteLine(current.Seconds);
+        var c = grid.Get(neighbor, Wall);
+        if (c == Wall) continue;
+        if (checkpoints.Contains(neighbor))
+        {
+          if (neighbor != current.Want) continue;
+          if (neighbor == S)
+          {
+            if (currentAltitude - 1 == startingAltitude)
+            {
+              (current.Seconds + 1).Should().Be(expected);
+              return;
+            }
+            continue;
+          }
+          else
+          {
+            nextWant = checkpoints[checkpoints.IndexOf(neighbor) + 1];
+          }
+        }
+
+        var nextAltitude = currentAltitude + c switch
+        {
+          Cold => -2,
+          Warm => +1,
+          _ => -1,
+        };
+        if (nextAltitude <= 0) continue;
+        open.Enqueue((neighbor, current.Seconds + 1, nextVector, nextWant, nextAltitude));
+      }
+      
+    }
+
+    throw new ApplicationException();
+  }
 
   private static Grid<char>  GetInput(string inputFile)
   {
